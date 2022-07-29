14f5edebca4c91cd7fe1bea3ba9b183c31d4d8a4
pashokman <pashokman1@gmail.com>
2022-07-29 21:13:55 +0300

    solution
    v2 optimisation

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 const fib = (n) => {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } else {
-        return fib(n - 1) + fib(n - 2);
-    }
-    
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fib(n - 1) + fib(n - 2);    
 };
 
 console.log(fib(3));  // 2
